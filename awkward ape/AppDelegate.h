7ea6fbc540d294ea238c2ee5d7ccfdf3e5ed74ca
Gaau <olegunnar@gmail.com>
2011-12-18 11:26:32 +0100

    Initial Commit

---
+++ awkward ape/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  awkward ape
+//
+//  Created by Ole Gunnar Tveit on 18.12.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window_;
+	RootViewController	*viewController_;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
---
+++ awkward ape/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  awkward ape
+//
+//  Created by Ole Gunnar Tveit on 18.12.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
